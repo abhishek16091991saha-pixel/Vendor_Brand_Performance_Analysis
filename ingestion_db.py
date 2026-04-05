2b7763a2c474b8b5a6ce920fef115062dd4b649d
abhishek16091991saha-pixel <abhishek16091991saha@gmail.com>
2026-04-06 01:56:54 +0530

    first commit

---
+++ ingestion_db.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine  
+
+import logging
+
+logging.basicConfig(
+    filename="logs/.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filemode="a",
+    force=True
+) 
+engine= create_engine('sqlite:///inventory.db')
+
+data_path = r"C:\Users\abhis\OneDrive\Documents\Vendor Analysis Project\data" 
+
+def ingest_db(df,table_name,engine):
+    '''this function will ingest the dataframe into database table'''
+    df.to_sql(table_name, con=engine, if_exists= 'replace',index= False) 
+
+def load_raw_data():
+    '''this function will load the CSVs as dataframe and ingest into db'''
+    start = time.time()
+    for file in os.listdir(data_path):
+        if file.endswith('.csv'):
+            full_path = os.path.join(data_path, file)
+            df = pd.read_csv(full_path)
+            logging.info(f'Ingesting {file} in db')
+            ingest_db(df, file[:-4],engine)
+    end = time.time()
+    total_time = (end - start)/60
+    logging.info('-------------Ingestion Complete-------------')
+    logging.info(f'\nTotal Time Taken: {total_time} minutes')        
+
+if __name__ == '__main__':
+    load_raw_data() 
